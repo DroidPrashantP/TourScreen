bb81c94c779f9b738d3fcfb1e44748f6f01db939
Prashant Patil <prashant@Prashants-MacBook-Pro.local>
2017-03-31 00:12:15 +0530

    My First commit

---
+++ TourScreen/Page.swift
@@ -0,0 +1,16 @@
+//
+//  Page.swift
+//  TourScreen
+//
+//  Created by Prashant Patil on 3/30/17.
+//  Copyright © 2017 Prashant Patil. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let title: String
+    let message: String
+    let imageName: String
+}
+
